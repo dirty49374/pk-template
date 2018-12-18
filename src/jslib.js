0f5f43fb6ea412f0b89b849e325a6d48c5e7ec2a
dirty49374 <dirty49374@gmail.com>
2018-12-18 23:12:22 +0900

    add examples

--- src/jslib.js
+++ src/jslib.js
@@ -1,12 +1,18 @@
+const path = require('path');
+const loaders = require('./loaders');
+
 const jslib = scope => ({
     add: object => scopes.add(scope, object),
     expand: path => statements.include(scope, { include: path }),
-    loadText: path => load.text(scope, url.resolve(scope.uri, path)),
-    loadYaml: path => path.toLowerCase().endsWith('.pkt')
-        ? jsyaml.load(load.text(scope, path), pktYamlOption)
-        : jsyaml.load(load.text(scope, path)),
-    loadYamlAll: path => jsyaml.loadAll(load.text(scope, path)),
-    loadTemplate: path => load.template(scope, path),
+    globs: (path, fromCwd) => loaders.globs(scope, path, fromCwd),
+    files: (path, fromCwd) => loaders.files(scope, path, fromCwd),
+    loadText: (path, fromCwd) => loaders.text(scope, path, fromCwd),
+    loadYaml: (path, fromCwd) => path.toLowerCase().endsWith('.pkt')
+        ? jsyaml.load(load.text(scope, path, fromCwd), pktYamlOption)
+        : jsyaml.load(load.text(scope, path, fromCwd)),
+    loadYamlAll: (path, fromCwd) => jsyaml.loadAll(load.text(scope, path, fromCwd)),
+    loadTemplate: (path, fromCwd) => load.template(scope, path, fromCwd),
+    basename: p => path.basename(p),
     label: (object, name) => {
         if (typeof object === 'string') {
             name = object
--- src/loaders.js
+++ src/loaders.js
@@ -1,6 +1,7 @@
 const _ = require('underscore');
 const fs = require("fs");
 const url = require('url');
+const glob = require('glob');
 const syncRequest = require('sync-request');
 
 const utils = require('./utils');
@@ -12,38 +13,64 @@ _.templateSettings = {
     evaluate: /\<\<\<\_(.+?)\>\>\>/g,
 };
 
-const load = {
-    text(scope, uri) {
+const loaders = {
+    resolve(scope, path, fromCwd) {
+        if (fromCwd) {
+            return path;
+        }
+        const p1 = scope ? scope.config.resolve(path) : path; // resolve @
+        const resolved = url.resolve(scope.uri, p1);
+        return resolved || '.';
+    },
+    isHttp(uri) {
+        const supportedProtocols = [ 'http:', 'https:' ];
+        const parsed = url.parse(uri);
+        return supportedProtocols.some(protocol => protocol == parsed.protocol);
+    },
+    text(scope, uri, fromCwd) {
         try {
-            uri = scope ? scope.config.resolve(uri) : uri; // resolve @
-            let parsed = url.parse(uri);
+            uri = loaders.resolve(scope, uri, fromCwd);
 
-            const supportedProtocols = [ 'http:', 'https:' ];
-            const isAbsoluteUrl = supportedProtocols.some(protocol => protocol == parsed.protocol);
-
-            return isAbsoluteUrl
+            return loaders.isHttp(uri)
                 ? syncRequest('GET', uri).getBody('utf8')
                 : fs.readFileSync(uri, 'utf8');
         } catch (e) {
             throw utils.pktError(scope, e, `failed to load ${uri}`);
         }
     },
-    yaml(scope, uri) {
-        const text = load.text(scope, uri);
+    yaml(scope, uri, fromCwd) {
+        const text = loaders.text(scope, uri, fromCwd);
         try {
             return yamls.loadAsPkt(text);
         } catch (e) {
             throw utils.pktError(scope, e, `failed to parse yaml ${uri}`);
         }
     },
-    template(scope, uri) {
-        const text = load.text(scope, uri);
+    template(scope, uri, fromCwd) {
+        const text = loaders.text(scope, uri, fromCwd);
         try {
             return _.template(text);
         } catch (e) {
             throw utils.pktError(scope, e, `failed to parse template ${uri}`);
         }
-    }
+    },
+    files(scope, path, fromCwd) {
+        const uri = loaders.resolve(scope, path, fromCwd);
+        if (loaders.isHttp(uri)) {
+            throw new Error(`cannot get directory listing from ${uri}`);
+        }
+
+        return fs.readdirSync(uri);
+    },
+    globs(scope, path, fromCwd) {
+        const uri = loaders.resolve(scope, path, fromCwd);
+        if (loaders.isHttp(uri)) {
+            throw new Error(`cannot get directory listing from ${uri}`);
+        }
+
+        const list = glob.sync(uri);
+        return list;
+    },
 }
 
-module.exports = load;
+module.exports = loaders;
