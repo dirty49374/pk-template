c4f4825ba04d38bf6d80684b6509848e72652267
dirtycode <dirty49374@gmail.com>
2019-03-25 21:01:19 +0900

    support multiple module update

--- src/pk/commands/module/updateCommand.ts
+++ src/pk/commands/module/updateCommand.ts
@@ -3,17 +3,35 @@ import { updateModule } from '../../../pk-conf/module';
 import { tryCatch, atProjectDir, atHomeDir } from '../../libs';
 import { IPkCommandInfo } from "../../types";
 import { PkConf } from '../../../pk-conf/conf';
+import { IPkModule } from '../../../pk-conf';
 
 export default (pk: IPkCommandInfo) => ({
-    command: 'update <module-name>',
+    command: 'update [module-name]',
     desc: 'update module from git repository',
     builder: (yargs: any) => yargs
         .option('branch', { alias: 'b', describe: 'branch name' })
         .option('tag', { alias: 't', describe: 'tag name' })
+        .option('all', { describe: 'all modules' })
         .option('global', { alias: 'g', describe: 'branch name', boolean: true }),
     handler: async (argv: any) => {
+        const update = async (mod: IPkModule) => {
+            console.log(`* updating ${mod.name} module...`)
+            if (argv.branch) {
+                mod.branch = argv.branch;
+                delete mod.tag;
+            } else if (argv.tag) {
+                mod.tag = argv.tag;
+                delete mod.branch;
+            } else {
+                mod.branch = 'master';
+            }
+            await updateModule(mod);
+        }
 
         await tryCatch(async () => {
+            if (!argv.moduleName && !argv.all) {
+                throw new Error("please specify module name or --all");
+            }
 
             const conf = PkConf.load();
             if (conf == null) {
@@ -22,43 +40,22 @@ export default (pk: IPkCommandInfo) => ({
 
             if (argv.global) {
                 await atHomeDir(async (dir) => {
-                    const mod = conf.data.modules
-                        .find((m: any) => m.name == argv.moduleName);
-                    if (!mod) {
-                        throw new Error(`can not find module ${argv.moduleName}`);
+                    for (const mod of conf.data.modules) {
+                        if (argv.all || mod.name == argv.moduleName) {
+                            await update(mod);
+                        }
                     }
-
-                    if (argv.branch) {
-                        mod.branch = argv.branch;
-                        delete mod.tag;
-                    } else if (argv.tag) {
-                        mod.tag = argv.tag;
-                        delete mod.branch;
-                    } else {
-                        mod.branch = 'master';
-                    }
-                    await updateModule(mod);
                     PkConf.save(conf);
                     console.log();
                 });
             } else {
                 await atProjectDir(async (projectRoot, projectConf) => {
-                    const mod = projectConf.data.modules
-                        .find((m: any) => m.name == argv.moduleName);
-                    if (!mod) {
-                        throw new Error(`can not find module ${argv.moduleName}`);
+                    for (const mod of projectConf.data.modules) {
+                        if (argv.all || mod.name == argv.moduleName) {
+                            await update(mod);
+                        }
                     }
 
-                    if (argv.branch) {
-                        mod.branch = argv.branch;
-                        delete mod.tag;
-                    } else if (argv.tag) {
-                        mod.tag = argv.tag;
-                        delete mod.branch;
-                    } else {
-                        mod.branch = 'master';
-                    }
-                    await updateModule(mod);
                     PkProjectConf.save(projectConf, '.');
                     console.log();
                 });
