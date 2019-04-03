0b2d1159dba0fa0454bbb969406fb230d1a25fb6
dirtycode <dirty49374@gmail.com>
2019-04-03 18:05:17 +0900

    add dep2 command

--- src/pk-template/jslib.ts
+++ src/pk-template/jslib.ts
@@ -3,94 +3,96 @@ import { parseKvps, parseList, pktError, sha256, repository, repositoryPath } fr
 import { IScope } from './types';
 import { log } from './logger';
 import { createHash } from 'crypto';
-import { dumpYamlSortedKey } from '../pk-yaml';
+import { dumpYamlSortedKey, parseYaml as ParseYaml, dumpYaml } from '../pk-yaml';
 import { execSync } from 'child_process';
 
 const jslib = (scope: IScope) => {
-    const lib = {
-        indent: () => scope.trace ? ''.padEnd(scope.trace.depth() * 2) : '',
-        envVar: (name: string) => process.env[name],
-        log: (...msg: any[]) => log(...msg),
-        last: () => scope.objects.length == 0 ? undefined : scope.objects[scope.objects.length - 1],
-        sha256: (obj: any, len?: number) => sha256(obj, len),
-        repository: (ref?: string) => repository(ref),
-        repositoryPath: (path: string) => repositoryPath(path),
-        files: (path: string) => scope.listFiles(path).data,
-        loadText: (path: string) => scope.loadText(path).data,
-        loadPkt: (path: string) => scope.loadPkt(path).data,
-        loadYaml: (path: string) => scope.loadYaml(path).data,
-        loadYamlAll: (path: string) => scope.loadYamlAll(path).data,
-        loadTemplate: (path: string) => scope.loadTemplate(path).data,
-        basename: (p: string) => path.basename(p),
-        label: (object: any | string, name: string | null) => {
-            if (name === null) {
-                name = object as string;
-                object = scope.object
-            }
-            if (!object) return undefined;
-            if (!object.metadata) return undefined;
-            if (!object.metadata.labels) return undefined;
-            return object.metadata.labels[name];
-        },
-        base64encode: (txt: string) => Buffer.from(txt).toString('base64'),
-        base64decode: (txt: string) => Buffer.from(txt, 'base64').toString('utf8'),
-        setlabel: (object: any, name: string, value: string) => {
-            if (typeof object === 'string') {
-                value = name;
-                name = object;
-                object = scope.object;
-            }
-            if (!object) throw pktError(scope, new Error('cannot set label'), 'object is empty');
-            if (!object.metadata) object.metadata = {};
-            if (!object.metadata.labels) object.metadata.labels = {};
-            object.metadata.labels[name] = value;
-        },
-        annotation: (object: any, name: string | undefined) => {
-            if (name == null) {
-                name = object as string;
-                object = scope.object;
-            }
-            if (!object) return undefined;
-            if (!object.metadata) return undefined;
-            if (!object.metadata.annotations) return undefined;
-            return object.metadata.annotations[name];
-        },
-        setannotation: (object: any, name: string, value: string | undefined) => {
-            if (typeof object === 'string') {
-                value = name
-                name = object
-                object = scope.object
-            }
-            if (!object) throw pktError(scope, new Error('cannot set annotation'), 'object is empty');
-            if (!object.metadata) object.metadata = {};
-            if (!object.metadata.annotations) object.metadata.annotations = {};
-            object.metadata.annotations[name] = value;
-        },
-        set: (node: any, path: string, value: any) => {
-            const _setValue = (node: any, pathes: string[], value: any) => {
-                if (true) {
-                    const key = pathes[0];
-                    if (pathes.length == 1) {
-                        node[key] = value;
-                    } else {
-                        const child = key in node ? node[key] : (node[key] = {});
-                        pathes.shift();
-                        _setValue(child, pathes, value);
-                    }
-                }
-            }
-            _setValue(node, path.split('.'), value);
-        },
-        add: (object: any) => scope.add(object),
-        arraify: (value: any) => Array.isArray(value) ? value : [value],
-        parseKvps,
-        parseList,
-        toWords: (value: string) => value.split(/\s+/).filter(p => p.length !== 0),
-        toNumbers: (value: string) => lib.toWords(value).map(p => Number(p)),
-        toList: (o: any, cb: any) => Object.keys(o).map(k => cb(k, o[k])),
-        styleMap: (list: any[]) => list.reduce((sum: any, kv: any) => { sum[kv.k] = kv.v; return sum; }, {}),
-    };
-    return lib;
+  const lib = {
+    indent: () => scope.trace ? ''.padEnd(scope.trace.depth() * 2) : '',
+    envVar: (name: string) => process.env[name],
+    log: (...msg: any[]) => log(...msg),
+    last: () => scope.objects.length == 0 ? undefined : scope.objects[scope.objects.length - 1],
+    sha256: (obj: any, len?: number) => sha256(obj, len),
+    repository: (ref?: string) => repository(ref),
+    repositoryPath: (path: string) => repositoryPath(path),
+    files: (path: string) => scope.listFiles(path).data,
+    loadText: (path: string) => scope.loadText(path).data,
+    loadPkt: (path: string) => scope.loadPkt(path).data,
+    loadYaml: (path: string) => scope.loadYaml(path).data,
+    loadYamlAll: (path: string) => scope.loadYamlAll(path).data,
+    loadTemplate: (path: string) => scope.loadTemplate(path).data,
+    basename: (p: string) => path.basename(p),
+    label: (object: any | string, name: string | null) => {
+      if (name === null) {
+        name = object as string;
+        object = scope.object
+      }
+      if (!object) return undefined;
+      if (!object.metadata) return undefined;
+      if (!object.metadata.labels) return undefined;
+      return object.metadata.labels[name];
+    },
+    base64encode: (txt: string) => Buffer.from(txt).toString('base64'),
+    base64decode: (txt: string) => Buffer.from(txt, 'base64').toString('utf8'),
+    setlabel: (object: any, name: string, value: string) => {
+      if (typeof object === 'string') {
+        value = name;
+        name = object;
+        object = scope.object;
+      }
+      if (!object) throw pktError(scope, new Error('cannot set label'), 'object is empty');
+      if (!object.metadata) object.metadata = {};
+      if (!object.metadata.labels) object.metadata.labels = {};
+      object.metadata.labels[name] = value;
+    },
+    annotation: (object: any, name: string | undefined) => {
+      if (name == null) {
+        name = object as string;
+        object = scope.object;
+      }
+      if (!object) return undefined;
+      if (!object.metadata) return undefined;
+      if (!object.metadata.annotations) return undefined;
+      return object.metadata.annotations[name];
+    },
+    setannotation: (object: any, name: string, value: string | undefined) => {
+      if (typeof object === 'string') {
+        value = name
+        name = object
+        object = scope.object
+      }
+      if (!object) throw pktError(scope, new Error('cannot set annotation'), 'object is empty');
+      if (!object.metadata) object.metadata = {};
+      if (!object.metadata.annotations) object.metadata.annotations = {};
+      object.metadata.annotations[name] = value;
+    },
+    set: (node: any, path: string, value: any) => {
+      const _setValue = (node: any, pathes: string[], value: any) => {
+        if (true) {
+          const key = pathes[0];
+          if (pathes.length == 1) {
+            node[key] = value;
+          } else {
+            const child = key in node ? node[key] : (node[key] = {});
+            pathes.shift();
+            _setValue(child, pathes, value);
+          }
+        }
+      }
+      _setValue(node, path.split('.'), value);
+    },
+    add: (object: any) => scope.add(object),
+    arraify: (value: any) => Array.isArray(value) ? value : [value],
+    parseKvps,
+    parseList,
+    parseYaml: (yaml: string) => ParseYaml(yaml, scope.uri),
+    dumpYaml: (obj: any) => dumpYaml(obj),
+    toWords: (value: string) => value.split(/\s+/).filter(p => p.length !== 0),
+    toNumbers: (value: string) => lib.toWords(value).map(p => Number(p)),
+    toList: (o: any, cb: any) => Object.keys(o).map(k => cb(k, o[k])),
+    styleMap: (list: any[]) => list.reduce((sum: any, kv: any) => { sum[kv.k] = kv.v; return sum; }, {}),
+  };
+  return lib;
 };
 
 export default jslib;
--- src/pk/index.ts
+++ src/pk/index.ts
@@ -12,63 +12,64 @@ import { compilePkt } from "../pk-template/languageSpec";
 import { IPkCommandInfo } from "./types";
 
 async function main(argv: string[]) {
-    const conf = PkConf.load();
-    const { projectRoot, projectConf } = PkProjectConf.find();
+  const conf = PkConf.load();
+  const { projectRoot, projectConf } = PkProjectConf.find();
 
-    const yargs = require('yargs')(argv)
-        .scriptName("pk")
+  const yargs = require('yargs')(argv)
+    .scriptName("pk")
 
-    const pk: IPkCommandInfo = {
-        ...libs, ...lazy, ui: cmdui, projectRoot, projectConf, generate, yaml, jsonpatch, compilePkt
-    };
-    if (projectConf && projectRoot) {
-        yargs
-            .command(require('./commands/app').default(pk))
-            .command(require('./commands/env').default(pk))
-            .command(require('./commands/deployment').default(pk))
-            ;
-
-        if (projectConf.data.modules) {
-            for (const mod of projectConf.data.modules) {
-                yargs.command({
-                    command: `${mod.name} <command>`,
-                    description: `module ${mod.name} command`,
-                    builder: (yargs: any) => {
-                        loadModuleCommands(yargs, projectRoot, mod.name, pk);
-                        loadModuleGenerators(yargs, projectRoot, mod.name, pk);
-                    }
-                });
-            }
-        }
-
-    } else {
-        yargs
-            .command(require('./commands/init').default(pk));
-    }
-
-    if (conf && conf.data && conf.data.modules) {
-        for (const mod of conf.data.modules) {
-            yargs.command({
-                command: `${mod.name} <command>`,
-                description: `module ${mod.name} command`,
-                builder: (yargs: any) => {
-                    loadModuleCommands(yargs, homedir(), mod.name, pk);
-                    loadModuleGenerators(yargs, homedir(), mod.name, pk);
-                }
-            });
-        }
-    }
-
+  const pk: IPkCommandInfo = {
+    ...libs, ...lazy, ui: cmdui, projectRoot, projectConf, generate, yaml, jsonpatch, compilePkt
+  };
+  if (projectConf && projectRoot) {
     yargs
-        .command(require('./commands/module').default(pk))
-        .command(require('./commands/config').default(pk))
-        .option('d', { description: 'enable stacktrace on error', boolean: true })
-        .recommendCommands()
-        .demandCommand()
-        .strict()
-        .help('h')
-        .argv
-        ;
+      .command(require('./commands/app').default(pk))
+      .command(require('./commands/env').default(pk))
+      .command(require('./commands/deployment').default(pk))
+      .command(require('./commands/deployment2').default(pk))
+      ;
+
+    if (projectConf.data.modules) {
+      for (const mod of projectConf.data.modules) {
+        yargs.command({
+          command: `${mod.name} <command>`,
+          description: `module ${mod.name} command`,
+          builder: (yargs: any) => {
+            loadModuleCommands(yargs, projectRoot, mod.name, pk);
+            loadModuleGenerators(yargs, projectRoot, mod.name, pk);
+          }
+        });
+      }
+    }
+
+  } else {
+    yargs
+      .command(require('./commands/init').default(pk));
+  }
+
+  if (conf && conf.data && conf.data.modules) {
+    for (const mod of conf.data.modules) {
+      yargs.command({
+        command: `${mod.name} <command>`,
+        description: `module ${mod.name} command`,
+        builder: (yargs: any) => {
+          loadModuleCommands(yargs, homedir(), mod.name, pk);
+          loadModuleGenerators(yargs, homedir(), mod.name, pk);
+        }
+      });
+    }
+  }
+
+  yargs
+    .command(require('./commands/module').default(pk))
+    .command(require('./commands/config').default(pk))
+    .option('d', { description: 'enable stacktrace on error', boolean: true })
+    .recommendCommands()
+    .demandCommand()
+    .strict()
+    .help('h')
+    .argv
+    ;
 }
 
 main(process.argv.slice(2));
